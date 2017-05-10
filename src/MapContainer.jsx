2acf009491b35744b6fc14e45587f7917bdc9aab
Roman Dubinin <romanonthego@gmail.com>
2017-05-10 14:47:03 +0700

    `react@15.5` and `prop-types` warning fix

--- src/MapContainer.jsx
+++ src/MapContainer.jsx
@@ -1,4 +1,5 @@
-import React, { Component, PropTypes } from 'react';
+import PropTypes from 'prop-types';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import MapElement from './MapElement';
 import MapController from './controllers/MapController';
